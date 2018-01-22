290a5bd7e62c9b6a47211973852dfe4ec8423020
Joppe Aarts <joppe@apestaartje.info>
2018-01-22 16:20:59 +0100

    Added support for cli argument to disable sandbox mode for puppeteer

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const chalk = require('chalk');
 const getColorName = require('./color-name/color-blindness.com').getColorName;
+let noSandbox = false;
 
 if (process.argv.length < 2) {
     console.log('Please provide a color in hexadecimal notation.');
     return;
 }
 
-if (process.argv.length > 3) {
+if (process.argv.length === 4) {
+    if (process.argv[3] === '--no-sandbox') {
+        noSandbox = true;
+    } else {
+        console.log(chalk.keyword('orange')('Invalid argument, skipping'))
+    }
+}
+
+if (process.argv.length > 4) {
     console.log(chalk.red.bold('Too many arguments.'));
     return;
 }
@@ -18,6 +27,6 @@ if (color.length !== 6) {
     return;
 }
 
-getColorName(color).then((colorName) => {
+getColorName(color, noSandbox).then((colorName) => {
     console.log(chalk.hex(color).bold('\u25A0'), `$${colorName}: #${color};`);
 });
